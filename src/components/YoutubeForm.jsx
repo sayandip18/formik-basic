242e3edbb476f166a4f4752b812a01b14bf65f9b
Sayandip Halder <sayandiph4@gmail.com>
2022-08-10 10:19:55 +0530

    use useFormik hook

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,22 +1,52 @@
 import React from 'react'
+import { useFormik } from 'formik'
 
 const YoutubeForm = () => {
-  return (
-    <div>
-        <form>
-            <label htmlFor='name'>Name</label>
-            <input type='text' id='name' name='name' />
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            channel: '',
+        },
+        onSubmit: values => {
+            console.log(values)
+        }
+    })
 
-            <label htmlFor='email'>E-mail</label>
-            <input type='email' id='email' name='email' />
+    return (
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor='name'>Name</label>
+                <input
+                    type='text'
+                    id='name'
+                    name='name'
+                    onChange={formik.handleChange}
+                    value={formik.values.name}
+                />
 
-            <label htmlFor='name'>Channel</label>
-            <input type='text' id='channel' name='channel' />
+                <label htmlFor='email'>E-mail</label>
+                <input
+                    type='email'
+                    id='email'
+                    name='email'
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
 
-            <button>Submit</button>
-        </form>
-    </div>
-  )
+                <label htmlFor='channel'>Channel</label>
+                <input
+                    type='text'
+                    id='channel'
+                    name='channel'
+                    onChange={formik.handleChange}
+                    value={formik.values.channel}
+                />
+
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default YoutubeForm
